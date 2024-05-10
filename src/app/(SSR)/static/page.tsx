d48ccc5ad4fac58b1d30b173b4ad270923ed5de5
ticoniq_ <okolietochi@gmail>
2024-05-10 02:00:05 +0100

    chore: Update next.config.js and layout.tsx

---
+++ src/app/(SSR)/static/page.tsx
@@ -0,0 +1,28 @@
+import { UnsplashImage } from "@/models/unsplash-image";
+import Image from "next/image";
+import Link from "next/link";
+
+export const metadata = {
+  title: 'Static Fetching - Next js image Gallery',
+}
+
+export default async function Page() {
+  const response = await fetch("https://api.unsplash.com/photos/random?client_id=" + process.env.UNSPLASH_aCCESS_KEY);
+  const image: UnsplashImage = await response.json();
+
+  const width = Math.min(500, image.width)
+  const height = (width / image.width) * image.height
+
+  return(
+    <div className="d-flex flex-column align-items-center">
+      <Image 
+        src={image.urls.raw}
+        width={width}
+        height={height}
+        alt={image.description}
+        className="rounded shadow mv-100 h-100"
+      />
+      by <Link href={"/users/" + image.user.username}>{image.user.username}</Link>
+    </div>
+  )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { Container, SSRProvider } from '@/components/bootstrap'
+import NavBar from './NavBar';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +18,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <SSRProvider>
+          <NavBar />
+          <main>
+            <Container className="py-4">
+              {children}
+            </Container>
+          </main>
+        </SSRProvider>
+      </body>
     </html>
   )
 }
